210b1e275d83e6cd50c3a3e275d55807eac826a7
jlooper <jen.looper@gmail.com>
2015-01-06 11:27:33 -0500

    adding kendo to the mix

---
+++ sample-app/public/javascripts/index.js
@@ -0,0 +1,13 @@
+(function() {
+
+	$("#timepicker").kendoTimePicker();
+	
+	  	var model = kendo.observable({
+
+  			//model goes here, including click actions
+   
+  		});
+
+  kendo.bind(document.body, model);
+
+}());
--- sample-app/public/javascripts/widgets.js
+++
@@ -1,2 +0,0 @@
-//timepicker
-$("#timepicker").kendoTimePicker()
